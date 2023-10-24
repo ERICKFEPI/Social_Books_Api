fda1b4dc1e2ee2717afbc55b14774ef587edc8fb
ERICKFEPI <00020934@fepi.edu.br>
2023-10-24 17:18:29 -0300

    lab03 continuacao

--- api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -1,14 +1,24 @@
 package br.fepi.socialbooks.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+import br.fepi.socialbooks.domain.Livro;
+import br.fepi.socialbooks.repository.LivrosRepository;
+
+@RestController
 public class LivrosResources {
 	
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
-	public String listar () {
-		return "O pequeno príncipe; Os segredos da mente milionária";
+	@Autowired
+	private LivrosRepository livrosRepository;
+	
+	@RequestMapping(value = "/livros",method = RequestMethod.GET)
+	public List<Livro> listar() {
+		return livrosRepository.findAll();
 	}
+
 }
