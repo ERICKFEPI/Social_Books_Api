2ef7711d57186f2e5fac94dfff0456f2ea5c457e
ERICKFEPI <00020934@fepi.edu.br>
2023-11-21 17:39:55 -0300

    Aula05_Comentarios

--- api_social_books/src/main/java/br/fepi/socialbooks/domain/Comentario.java
+++ api_social_books/src/main/java/br/fepi/socialbooks/domain/Comentario.java
@@ -2,8 +2,23 @@ package br.fepi.socialbooks.domain;
 
 import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity 
+
 public class Comentario {
 	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String texto;
@@ -11,6 +26,11 @@ public class Comentario {
 	private String usuario;
 	
 	private Date data;
+	
+	@ManyToOne (fetch = FetchType.LAZY)
+	@JoinColumn (name = "LIVRO_ID")
+	@JsonIgnore
+	private Livro livro;
 
 	public Long getId() {
 		return id;
